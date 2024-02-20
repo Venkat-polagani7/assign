baf3a271eb3d3493abcb751f98a9a1e12b2124a5
Venkat_Polagani <103422239+Venkat-polagani7@users.noreply.github.com>
2024-02-20 10:08:45 +0530

    Add files via upload

---
+++ lib/cached_network_images/CachedNetworkImage.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:cached_network_image/cached_network_image.dart';
+import 'package:assignment_0/data_provider/DataProvider.dart';
+
+class ImageGrid extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final dataProvider = Provider.of<DataProvider>(context);
+    final images = dataProvider.images;
+
+    return GridView.builder(
+      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+        crossAxisCount: 2,
+        crossAxisSpacing: 4.0,
+        mainAxisSpacing: 4.0,
+      ),
+      itemCount: images.length,
+      itemBuilder: (ctx, index) {
+        return CachedNetworkImage(
+          imageUrl: images[index]['url'],
+          placeholder: (context, url) => CircularProgressIndicator(),
+          errorWidget: (context, url, error) => Icon(Icons.error),
+        );
+      },
+    );
+  }
+}
+
+
